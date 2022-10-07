9edae8a0232499ee1a5c6a91c016509d96dcb46e
VikralAIT <m.vikral.arridho@akarinti.tech>
2022-10-07 10:32:27 +0700

    do over

--- lib/features/auth/domain/repositories/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ait_flutter_template/core/exception/failure.dart';
-import 'package:ait_flutter_template/features/auth/domain/entities/userentity.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class IAuthRepository {
-  Future<Either<Failure, UserEntity>> login();
-  Future<Either<Failure, Unit>> logout();
-  Future<Either<Failure, UserEntity>> autoLogin();
-}
