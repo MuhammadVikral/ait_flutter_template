bf52c2576772bc935e33ecf22cc6a776e6042504
VikralAIT <m.vikral.arridho@akarinti.tech>
2022-11-11 15:53:45 +0700

    feature(auth) : add username input widget test

--- packages/auth/test/src/presentation/pages/login/widgets/username_input_test.dart
+++ packages/auth/test/src/presentation/pages/login/widgets/username_input_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final loginButton = find.byKey(const ValueKey('login button'));
   final usernameInput = find.byKey(const ValueKey('username input'));
   final passwordInput = find.byKey(const ValueKey('password input'));
   bool showErrorMessage = false;
@@ -26,6 +25,68 @@ void main() {
       await tester.tap(usernameInput);
       await tester.tap(passwordInput);
       expect(showErrorMessage, true);
+    },
+  );
+  testWidgets(
+    'input username should not show error message on start',
+    (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: Login()));
+      expect(
+        (tester.firstWidget(usernameInput) as DesignTextInput).errorText,
+        null,
+      );
+    },
+  );
+  testWidgets(
+    'input username should show error message when username input loses focused',
+    (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: Login()));
+      await tester.tap(usernameInput);
+      await tester.tap(passwordInput);
+      await tester.pump();
+      expect(
+        (tester.firstWidget(usernameInput) as DesignTextInput).errorText,
+        'Tidak boleh kosong',
+      );
+    },
+  );
+  testWidgets(
+    'input username should show error message "Format email Salah" when user inputted email with wrong format',
+    (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: Login()));
+      await tester.enterText(usernameInput, '@123');
+      await tester.tap(passwordInput);
+      await tester.pump();
+      expect(
+        (tester.firstWidget(usernameInput) as DesignTextInput).errorText,
+        'Format email Salah',
+      );
+    },
+  );
+  testWidgets(
+    'input username should show error message "format phone number tidak sesuai" when user inputted phone number with wrong format',
+    (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: Login()));
+      await tester.enterText(usernameInput, '123');
+      await tester.tap(passwordInput);
+      await tester.pump();
+      expect(
+        (tester.firstWidget(usernameInput) as DesignTextInput).errorText,
+        'format phone number tidak sesuai',
+      );
+    },
+  );
+  testWidgets(
+    'input username should show error message "Please enter at least 3 characters." when user inputted username with wrong format',
+    (tester) async {
+      await tester.pumpWidget(const MaterialApp(home: Login()));
+      await tester.enterText(usernameInput, 'ab');
+      await tester.tap(passwordInput);
+      await tester.pump();
+      expect(
+        (tester.firstWidget(usernameInput) as DesignTextInput).errorText,
+        'Please enter at least 3 characters.',
+      );
     },
   );
 }
